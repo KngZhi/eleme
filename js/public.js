bce720b3aebb3395d1ccd06dd4f1380eecd03bdb
lingmengjia <2297848169@qq.com>
2017-08-05 17:02:04 +0800

    save

---
+++ js/public.js
@@ -0,0 +1,13 @@
+//根字号初始化
+initSize();
+function initSize(){
+	var  screenW = document.documentElement.clientWidth || document.body.clientWidth;
+	var oHtml = document.getElementsByTagName('html')[0]
+	var size = 100 * screenW / 640 + 'px'
+	oHtml.style.fontSize = size;
+}
+
+//点击返回页面事件
+function returnPages(){
+	window.history.go(-1);
+}
